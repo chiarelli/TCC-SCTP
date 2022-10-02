e4452d26e06c3ad7537510df6034cf6e541644b9
Raphael <oi.sucesso@gmail.com>
2022-10-02 02:38:33 -0300

    Created UserService

---
+++ microservices/src/service/UserService/index.ts
@@ -0,0 +1,35 @@
+import { ServiceBroker } from 'moleculer';
+import { Microservice } from '../interfaces';
+import { connect } from '../mongodb-conn';
+
+export class UserService implements Microservice {
+
+    constructor(private broker: ServiceBroker) { }
+    
+    async register() {
+        // Connect MongoDB
+        await connect();
+
+        // Define a service
+        this.broker.createService({
+            name: 'user',
+            actions: {
+                // createWorkspace: ctx => wksCrtl.create(ctx),
+                // updateWorkspace: ctx => wksCrtl.update(ctx),
+                // deleteWorkspace: ctx => wksCrtl.delete(ctx),
+                // getWorkspace   : ctx => wksCrtl.getOne(ctx),
+                // getAllWorkspace: ctx => wksCrtl.getAll(ctx),
+            }
+        });
+        
+        // graceful shutdown service
+        process.on('SIGINT', async () => {
+            console.log(`##### graceful shutdown service ${this.broker.nodeID}`);
+            await this.broker.stop();
+        });
+
+        // Start the broker
+        return this.broker.start();
+    }
+
+}
