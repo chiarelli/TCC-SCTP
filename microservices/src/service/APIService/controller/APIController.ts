2f25b6a37e263e494db1ce60aee48bde5328a6d9
Raphael <oi.sucesso@gmail.com>
2022-10-02 02:48:21 -0300

    API actions workspace CRUD broker call user service

--- microservices/src/service/APIService/controller/APIController.ts
+++ microservices/src/service/APIService/controller/APIController.ts
@@ -5,4 +5,31 @@ export class APIController {
 
     constructor(private broker: ServiceBroker){ }
     
+    getAllWorkspace(ctx: Context): any {
+        return this.broker.call('user.getAllWorkspace', ctx.params);
+    }
+
+    getWorkspace(ctx: Context): any {
+        return this.broker.call('user.getWorkspace', ctx.params);
+    }
+
+    createWorkspace(ctx: Context): any {
+        ctx.meta.$statusCode = 201;      
+        return this.broker.call('user.createWorkspace', ctx.params);
+    }
+
+    updateWorkspace(ctx: Context): any {
+        return this.broker.call('user.updateWorkspace', ctx.params);
+    }
+
+    async deleteWorkspace(ctx: Context): Promise<any> {
+        try {
+            await this.broker.call('user.deleteWorkspace', ctx.params);
+        } catch(e) {
+
+        }
+        ctx.meta.$statusCode = 204;
+        return '';
+    }
+    
 }
