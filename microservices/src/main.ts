0b421f55a039b3c70d67bc385b2f508ee76fee08
Raphael <oi.sucesso@gmail.com>
2022-10-02 02:54:08 -0300

    Start services APIService and UserService

--- microservices/src/main.ts
+++ microservices/src/main.ts
@@ -1,9 +1,28 @@
-function sayMyName(name: string): void {
-    if (name === "Heisenberg") {
-      console.log("You're right 👍");
-    } else {
-      console.log("You're wrong 👎");
-    }
+import { AvailableServices, MicroserviceConstructor } from './service/interfaces';
+import { APIService } from './service/APIService';
+import { ServiceBrokerDefaultFactory as brokerDefault } from './service/factories';
+import { UserService } from './service/UserService';
+
+class Services {
+
+  static readonly requestedServices: Array<string> = process.env.SERVICES?.split('|') || [];
+
+  static readonly availableServices: AvailableServices = {
+    api: APIService,
+    user: UserService
   }
-   
-  sayMyName("Heisenberg");
+  
+  private services: Array<Function> = [];
+  
+  addService(serviceClass: MicroserviceConstructor) {
+    this.services.push(() => (new serviceClass(brokerDefault.getNewInstance())).register());
+  }
+
+  start() {
+    Services.requestedServices.forEach(service => this.addService(Services.availableServices[service]));
+    this.services.forEach(start => start());
+  }
+
+}
+
+(new Services).start();
