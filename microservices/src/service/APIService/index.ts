066378b4509955c4c6d2342479477ca93a80db88
Raphael <oi.sucesso@gmail.com>
2022-10-02 02:51:40 -0300

    Registed routes workspace CRUD

--- microservices/src/service/APIService/index.ts
+++ microservices/src/service/APIService/index.ts
@@ -1,6 +1,7 @@
 import { ServiceBroker } from "moleculer";
 import { Microservice } from "../interfaces";
 import { connect } from "../mongodb-conn";
+import { APIController } from "./controller/APIController";
 import web from "moleculer-web";
 
 export class APIService implements Microservice {
@@ -12,27 +13,28 @@ export class APIService implements Microservice {
         await connect();
 
         // Define a service
+        const apiCtrl = new APIController(this.broker);
 
         this.broker.createService({
             name: 'api',
             mixins: [web],
             actions: {
-                // createWorkspace: ctx => apiCtrl.createWorkspace(ctx),
-                // updateWorkspace: ctx => apiCtrl.updateWorkspace(ctx),
-                // deleteWorkspace: ctx => apiCtrl.deleteWorkspace(ctx),
-                // getWorkspace: ctx => apiCtrl.getWorkspace(ctx),
-                // getAllWorkspace: ctx => apiCtrl.getAllWorkspace(ctx),
+                createWorkspace: ctx => apiCtrl.createWorkspace(ctx),
+                updateWorkspace: ctx => apiCtrl.updateWorkspace(ctx),
+                deleteWorkspace: ctx => apiCtrl.deleteWorkspace(ctx),
+                getWorkspace: ctx => apiCtrl.getWorkspace(ctx),
+                getAllWorkspace: ctx => apiCtrl.getAllWorkspace(ctx),
             },
             settings: {
                 routes: [{
                     path: '/api',
                     aliases: {
                         // Resource workspace routers
-                        // 'POST workspace': 'api.createWorkspace',
-                        // 'PUT workspace/:id': 'api.updateWorkspace',
-                        // 'DELETE workspace/:id': 'api.deleteWorkspace',
-                        // 'GET workspace/:id': 'api.getWorkspace',
-                        // 'GET workspace': 'api.getAllWorkspace',
+                        'POST workspace': 'api.createWorkspace',
+                        'PUT workspace/:id': 'api.updateWorkspace',
+                        'DELETE workspace/:id': 'api.deleteWorkspace',
+                        'GET workspace/:id': 'api.getWorkspace',
+                        'GET workspace': 'api.getAllWorkspace',
                     }
                 }]
             }
