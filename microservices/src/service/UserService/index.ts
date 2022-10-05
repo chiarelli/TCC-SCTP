064f80f5c601d04f7f1c1edcb81d12d7ee890717
Raphael <oi.sucesso@gmail.com>
2022-10-05 17:02:10 -0300

    Registered admin actions

--- microservices/src/service/UserService/index.ts
+++ microservices/src/service/UserService/index.ts
@@ -1,7 +1,7 @@
 import { ServiceBroker } from 'moleculer';
 import { Microservice } from '../interfaces';
 import { connect } from '../mongodb-conn';
-import { WorkspaceCtrlSingleton } from './user-factories';
+import { AdminCtrlSingleton, WorkspaceCtrlSingleton } from './user-factories';
 
 export class UserService implements Microservice {
 
@@ -12,16 +12,25 @@ export class UserService implements Microservice {
         await connect();
 
         const wksCrtl = WorkspaceCtrlSingleton.getInstance();
+        const adminCtrl = AdminCtrlSingleton.getInstance();
         
         // Define a service
         this.broker.createService({
             name: 'user',
             actions: {
+                // Resource workspace actions
                 createWorkspace: ctx => wksCrtl.create(ctx),
                 updateWorkspace: ctx => wksCrtl.update(ctx),
                 deleteWorkspace: ctx => wksCrtl.delete(ctx),
                 getWorkspace   : ctx => wksCrtl.getOne(ctx),
                 getAllWorkspace: ctx => wksCrtl.getAll(ctx),
+                
+                // Resource admin actions
+                createAdmin: ctx => adminCtrl.create(ctx),
+                updateAdmin: ctx => adminCtrl.update(ctx),
+                deleteAdmin: ctx => adminCtrl.delete(ctx),
+                getAdmin   : ctx => adminCtrl.getOne(ctx),
+                getAllAdmin: ctx => adminCtrl.getAll(ctx),
             }
         });
         
--- microservices/src/service/UserService/user-factories.ts
+++ microservices/src/service/UserService/user-factories.ts
@@ -1,26 +1,52 @@
+import { AdminController } from "./controller/AdminController";
 import { WorkspaceController } from "./controller/WorkspaceController";
+import { AdminModel } from "./model/AdminModel";
 import { WorkspaceModel } from "./model/WokspaceModel";
 
 export class WorkspaceCtrlSingleton {
-    private static tokenModel: WorkspaceController;
+    private static ctrl: WorkspaceController;
 
     static getInstance(): WorkspaceController {
         const _static = WorkspaceCtrlSingleton;
-        if(!_static.tokenModel) {
-            _static.tokenModel = new WorkspaceController(WorkspaceModelSingleton.getInstance());
+        if(!_static.ctrl) {
+            _static.ctrl = new WorkspaceController(WorkspaceModelSingleton.getInstance());
         }
-        return _static.tokenModel;
+        return _static.ctrl;
+    }
+}
+
+export class AdminCtrlSingleton {
+    private static ctrl: AdminController;
+
+    static getInstance(): AdminController {
+        const _static = AdminCtrlSingleton;
+        if(!_static.ctrl) {
+            _static.ctrl = new AdminController(AdminModelSingleton.getInstance());
+        }
+        return _static.ctrl;
     }
 }
 
 export class WorkspaceModelSingleton {
-    private static tokenModel: WorkspaceModel;
+    private static model: WorkspaceModel;
 
     static getInstance(): WorkspaceModel {
         const _static = WorkspaceModelSingleton;
-        if(!_static.tokenModel) {
-            _static.tokenModel = new WorkspaceModel;
+        if(!_static.model) {
+            _static.model = new WorkspaceModel;
         }
-        return _static.tokenModel;
+        return _static.model;
+    }
+}
+
+export class AdminModelSingleton {
+    private static model: AdminModel;
+
+    static getInstance(): AdminModel {
+        const _static = AdminModelSingleton;
+        if(!_static.model) {
+            _static.model = new AdminModel;
+        }
+        return _static.model;
     }
 }
