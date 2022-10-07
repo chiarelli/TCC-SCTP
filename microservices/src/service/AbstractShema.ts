cef14972250c231ed673358a7b6a3586def84d61
Raphael <oi.sucesso@gmail.com>
2022-10-07 00:55:48 -0300

    Clean code

--- microservices/src/service/AbstractShema.ts
+++ microservices/src/service/AbstractShema.ts
@@ -5,9 +5,7 @@ import { IModel } from './interfaces';
 export const AbstractShema = new Schema<IModel>({
     uuid: { type: Buffer, immutable: true, unique: true, required: true,
 
-        get: function (buffer: Buffer) {
-            return hexToUuid( buffer.toString('hex') );
-        }
+        get: (buffer: Buffer) => hexToUuid( buffer.toString('hex') )
     },
     createdAt: { type: Date, required: true },
     updatedAt: { type: Date, default: Date.now, required: true },
