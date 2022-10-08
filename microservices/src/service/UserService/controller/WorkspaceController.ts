f91f32550c2ad8ea888e22bac4b41e1cb6ac7f2a
Raphael <oi.sucesso@gmail.com>
2022-10-08 01:28:55 -0300

    Check permissions workspace CRUD

--- microservices/src/service/UserService/controller/WorkspaceController.ts
+++ microservices/src/service/UserService/controller/WorkspaceController.ts
@@ -1,15 +1,26 @@
-import { Context, IUser } from "../../interfaces";
+import { ServiceError } from "../../../error/ServiceError";
+import { Context, IUser, Permission } from "../../interfaces";
 import { WorkspaceModel } from "../model/WokspaceModel";
 import { AbstractController } from "./AbstractController";
 
 export class WorkspaceController extends AbstractController<IUser> {
 
-    async checkPermission(ctx: Context): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
-
     constructor(ctrl: WorkspaceModel) {
         super(ctrl);
+    }
+
+    async checkPermission(ctx: Context): Promise<void | never> {
+        if( !ctx.action?.name ) {
+            throw new ServiceError('action_not_defined');
+        }
+        const per: Permission = {
+            action: ctx.action.name,
+            loggedIn: ctx.user,
+            params: ctx.params,
+        }
+        if( ! await this.ctrl.checkPermission(per) ) {
+            throw new ServiceError('401 Unauthorized', 401);
+        }
     }
 
 }
--- microservices/src/service/UserService/model/WokspaceModel.ts
+++ microservices/src/service/UserService/model/WokspaceModel.ts
@@ -1,4 +1,5 @@
-import { Context, IUser } from "../../interfaces";
+import { IUser, Permission } from "../../interfaces";
+import { UUID_Utilities } from "../../UUID_Utilities";
 import { AbstractModel } from "./AbstractModel";
 import { Workspace } from "./user-schema";
 
@@ -8,16 +9,89 @@ export class WorkspaceModel extends AbstractModel<IUser> {
         super(Workspace);
     }
 
-    async checkPermission(ctx: Context): Promise<void> {
+    async checkPermission(permision: Permission): Promise<boolean> {
+        const user = permision.loggedIn;
+        const perms: Promise<boolean>[] = [];
+        // console.log('###### permision ----> ', permision)
+        if(!user) return false;
 
-        switch(ctx.action?.name) {
-            case 'user.createAdmin':
+        switch(permision.action) {
+
+            case 'user.deleteWorkspace':
+
+                perms.push(this.stubs.authService.checkPermission(
+                    { role: user.kind, actions: { name: 'deleteAny', resource: 'user' } }
+                ));
 
                 break;
 
-            default:
+            case 'user.getAllWorkspace':
+
+                perms.push(this.stubs.authService.checkPermission(
+                    { role: user.kind, actions: { name: 'readAny', resource: 'user' } }
+                ));
+
+                break;
+
+            case 'user.createWorkspace':
+
+                perms.push(this.stubs.authService.checkPermission(
+                    { role: user.kind, actions: { name: 'createAny', resource: 'user' } }
+                ));
+
+                break;
+
+            case 'user.getWorkspace':
+
+                const result1 = await this.stubs.authService.checkPermission(
+                    { role: user.kind, actions: { name: 'readAny', resource: 'user' } }
+                )
+
+                if(result1) { perms.push(Promise.resolve(true)); break; }
+
+                let uuid = typeof user.uuid === 'string' ? user.uuid : Buffer.from(user.uuid).toString('hex');
+                let resourceId = new String(permision.params.id).toString();
+                const result2 = uuid == UUID_Utilities.uuidToBuffer(resourceId).toString('hex')
+
+                if(!result2) { perms.push(Promise.resolve(false)); break; }
+
+                perms.push(
+                    this.stubs.authService.checkPermission(
+                        { role: user.kind,
+                            actions: { name: 'readOwn', resource: 'profile:workspace' }
+                        }
+                    )
+                )
+
+                break;
+
+            case 'user.updateWorkspace':
+
+                const result3 = await this.stubs.authService.checkPermission(
+                    { role: user.kind, actions: { name: 'updateAny', resource: 'user' } }
+                )
+
+                if(result3) { perms.push(Promise.resolve(true)); break; }
+
+                let uuid2 = typeof user.uuid === 'string' ? user.uuid : Buffer.from(user.uuid).toString('hex');
+                let resourceId2 = new String(permision.params.id).toString();
+                const result4 = uuid2 == UUID_Utilities.uuidToBuffer(resourceId2).toString('hex')
+
+                if(!result4) { perms.push(Promise.resolve(false)); break; }
+
+                perms.push(
+                    this.stubs.authService.checkPermission(
+                        { role: user.kind,
+                            actions: { name: 'updateOwn', resource: 'profile:workspace' }
+                        }
+                    )
+                )
+
+                break;
+
         }
 
+        return !!(await Promise.all(perms)).filter( item => item ).length
     }
 
 }
