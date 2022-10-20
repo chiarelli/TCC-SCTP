67d8cc47607ba317f30463f52f4447fbea396149
Raphael Mathias <oi.sucesso@gmail.com>
2022-10-19 23:12:00 -0300

    Created enum EvaluateStatuses

--- microservices/src/service/enums.ts
+++ microservices/src/service/enums.ts
@@ -8,3 +8,9 @@ export enum UserTypes {
     workspace = 'workspace',
     consumer = 'consumer',
 }
+
+export enum EvaluateStatuses {
+    approved= 'approved',
+    disapproved= 'disapproved',
+    none= 'none',
+}
