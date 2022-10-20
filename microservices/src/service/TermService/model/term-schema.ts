53b21488e9fd8e06a20b8795d51b6cb5a3c72517
Raphael Mathias <oi.sucesso@gmail.com>
2022-10-19 23:12:58 -0300

    Defined Term shema

---
+++ microservices/src/service/TermService/model/term-schema.ts
@@ -0,0 +1,19 @@
+import { Document, model, Schema } from "mongoose";
+import { AbstractShema } from "../../AbstractShema";
+import { EvaluateStatuses, Statuses } from "../../enums";
+import { ITerm } from "../../interfaces";
+
+export const TermSchema = new Schema<ITerm>({
+    ...AbstractShema.obj,
+    author: { type: Buffer, required: true },
+    slug: { type: String, maxLength: 100, unique: true },
+    name: { type: String, trim: true, maxLength: 100 },
+    question: { type: String, trim: true, maxLength: 100 },
+    description: { type: String, trim: true, maxLength: 600 },
+    evaluate: { type: String, enum: Object.values(EvaluateStatuses), default: EvaluateStatuses.none },
+    status: { type: String, required: true, enum: Object.values(Statuses), default: Statuses.active },
+});
+
+export const Term = model<ITerm>('Term', TermSchema);
+
+export type DocumentTermType = Document<unknown, any, ITerm> & ITerm;
